17cc5fbed09918405535ccc20af5f1ff4402dfc6
Muhd. Zulhilmi Abdullah <muhd.zulhilmi@makna.org.my>
2018-08-07 16:21:10 +0800

    showblade

--- resources/views/ter/show.blade.php
+++ resources/views/ter/show.blade.php
@@ -1,176 +1,153 @@
-@extends('layouts.app')
-
-@section('content')
-
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html>
 <head>
-	<title></title>
+    <title>MAKNA TAX Receipt</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+
 </head>
-<body>
+<body style="max-width: 100%; width: 1080px; margin:auto; padding: 20px; ">
 
-
-<h1>{{$prints->account_holder}}</h1>
-
-
-<!-- <div class="modal-content" style="color:#3377A7;padding:10px">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title-edit">Transaction detail</h4>
-        </div>
-        <div class="tdm_modal-body" style>
-          <div id="tdm_modal_error" style="color:red"></div>
-          <input type="hidden" id="tdm_tmp_attn_name">
-          <input type="hidden" id="tdm_tmp_attn_desi">
-
-          <div class="table-responsive">
-            <table class="table" style="color:#3377a7">
-              <tbody class="applicant_detail">
-                <tr>
-                  <td><label class="control-label" for="receipt_for">Tax Receipt For:</label></td>
-                  <td>
-                    <select class="form-control" name="receipt_for" id="tdm_receipt_for">
-                      <option>Individual</option>
-                      <option>Corporate</option>
-                    </select>
-                  </td>
-                </tr>
-                <tr class="attn_company row1">
-                </tr>
-                <tr class="attn_company row2">
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="account_holder">Donor:</label></td>
-                  <td><input class="form-control" type="text" id="tdm_account_holder"></td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="nric">NRIC/Passport:</label></td>
-                  <td><input class="form-control modal_error" type="text" id="tdm_nric"></td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="project">Project:</label></td>
-                  <td>
-                    <select class="form-control" name="tdm_project" id="tdm_project">
-                    </select>
-                  </td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="add1">Add1:</label></td>
-                  <td><input class="form-control modal_error" type="text" id="tdm_add1"></td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="add2">Add2:</label></td>
-                  <td><input class="form-control" type="text" id="tdm_add2"></td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="add3">Add3:</label></td>
-                  <td><input class="form-control" type="text" id="tdm_add3"></td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="add4">Add4:</label></td>
-                  <td><input class="form-control" type="text" id="tdm_add4"></td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="postcode">Postcode:</label></td>
-                  <td><input class="form-control modal_error" type="number" id="tdm_postcode" min="10000" step="1"></td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="state">State:</label></td>
-                  <td>
-                    <select class="form-control" name="state" id="tdm_state">
-                      <option>Kuala Lumpur</option>
-                      <option>Johor</option>
-                      <option>Kedah</option>
-                      <option>Kelantan</option>
-                      <option>Labuan</option>
-                      <option>Melaka</option>
-                      <option>Negeri Sembilan</option>
-                      <option>Pahang</option>
-                      <option>Penang</option>
-                      <option>Perak</option>
-                      <option>Perlis</option>
-                      <option>Putrajaya</option>
-                      <option>Sabah</option>
-                      <option>Sarawak</option>
-                      <option>Selangor</option>
-                      <option>Terengganu</option>
-                    </select>
-                  </td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="trans_date">Transaction Date:</label></td>
-                  <td><input class="form-control" type="text" id="trans_date" readOnly></td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="amount">Amount:</label></td>
-                  <td><input class="form-control" type="number" id="tdm_amount"  min="0" step="0.01"></td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="channel">Channel:</label></td>
-                  <td>
-                    <select class="form-control" name="tdm_channel" id="tdm_channel">
-                      <option>N/A</option>
-                      <option>Affin Bank</option>
-                      <option>Alliance Bank</option>
-                      <option>AmBank</option>
-                      <option>Asian Finance Bank</option>
-                      <option>Bank Islam</option>
-                      <option>Bank Muamalat</option>
-                      <option>Bank Rakyat</option>
-                      <option>BillPlz</option>
-                      <option>BSN</option>
-                      <option>CIMB</option>
-                      <option>Citi Bank</option>
-                      <option>Deutsche Bank</option>
-                      <option>Hong Leong Bank</option>
-                      <option>HSBC</option>
-                      <option>Maybank</option>
-                      <option>OCBC</option>
-                      <option>Paypal</option>
-                      <option>PeopleGiving</option>
-                      <option>Public Bank</option>
-                      <option>RHB</option>
-                      <option>SimplyGiving</option>
-                      <option>Standard Chartered Bank</option>
-                      <option>UOB</option>
-                    </select>
-                  </td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="payment_mode">Payment Mode:</label></td>
-                  <td>
-                    <select class="form-control" name="tdm_payment_mode" id="tdm_payment_mode">
-                      <option>ATM/Counter</option>
-                      <option>Cash</option>
-                      <option>Cheque</option>
-                      <option>Credit Card</option>
-                      <option>Malaysian Postal Order</option>
-                      <option>Online Transfer</option>
-                    </select>
-                  </td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="remarks">Remarks:</label></td>
-                  <td><input class="form-control" type="text" id="tdm_remarks"></td>
-                </tr>
-                <tr>
-                  <td><label class="control-label" for="email">Donor's Email:</label></td>
-                  <td><input class="form-control" type="text" id="tdm_email"></td>
-                </tr>
-              </tbody>
-            </table>
+    <div>
+        <img align="right" src="http://makna.org.my/wp-content/uploads/2013/04/MAKNA-Logo-Colour-Small.jpg" height="100" width="100" style="margin-top: -5px; margin-right:10px;"/>
+        <div style="line-height:6px;font-size:smaller;font-weight:600;font-style: italic;">
+            <p style="margin-bottom:25px">$todate</p>
+            <p>{{$account_holder}}</p>
+            <p id="pre_add1">{{$add1}}</p>
+            <p id="pre_add2">{{$add2}}</p>
+            <p id="pre_add23">{{$add3}}</p>
+            <p id="pre_add3">{{$postcode}}, {{$add4}}, {{strtoupper($state)}}</p>
+            <p id="pre_attn" style="margin-top:15px;"></p>
+            <br>
+    </div>
+    <div style="font-size:14px; text-align: justify; text-justify: inter-word;">
+            <p>Dear {{strtoupper($account_holder)}}</p>
+            <p style="color:#d8609e; font-size: medium;"><b> THANK YOU FOR YOUR KINDNESS</b></p>
+            <p>{{$project_text}}</p> 
           </div>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-primary save_edited_ter">Save</button>
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+        <p align="left" style="margin-top:50px;"><img src="http://makna.org.my/images/dato_chop.jpg" height="90" width="280"/></p>
+          <p align="left" style="margin-top:10px; margin-right: 25px;">
+            Dato’ Mohd Farid Ariffin<br>
+            Founder & President, MAKNA<br>
+          </p>
+
+           <p style="border-bottom: 2px dashed #000000;margin-top:1px;"></p>
+        <div style="font-size:smaller">
+        <img align="left" src="http://makna.org.my/wp-content/uploads/2013/04/MAKNA-Logo-Colour-Small.jpg" height="100" width="100" style="margin-top: -5px; margin-bottom:5px;"/>
+          <div style="line-height:6px;font-size:smaller;font-weight:600;font-style: italic;">
+          <p style="font-size:16px; color:#000; margin-right: 100px;"  align="right" >{{$trans_year}} TAX EXEMPT RECEIPT</p>
+            <p style="margin-top:20px;"align="right">
+            Receipt No: {{$id}}<span id="receipt_copy"></span><br><br>
+            Date of Donation: {{$trans_date}}</p>
         </div>
+
+        <div align:"left" style="font-size:smaller">
+          <table style="width:100%">
+            <tr style="height:40px;">
+              <td style="width:30%;">Diterima dari<br>
+                      <i>Received from</i></td>
+              <td style="border-bottom: 1px solid #000000;">{{strtoupper($account_holder)}}</td>
+            </tr>
+
+             <tr style="height:40px;">
+              <td style="width:30%;" id="nric_title">No. Kad Pengenalan/Pasport<br>
+                      <i>NRIC/Passport</i></td>
+             <td style="border-bottom: 1px solid #000000;" id="nric">{{strtoupper($ic)}}</td>
+            </tr>
+
+            <tr style="height:40px;">
+              <td style="width:30%;">sebanyak Ringgit Malaysia<br>
+                  <i>amount of Ringgit Malaysia</i></td>
+              <td style="border-bottom: 1px solid #000000;">{{$amount_in_words}}</td>
+            </tr>
+
+            <tr style="height:40px;">
+              <td style="width:30%;">untuk<br><i>for</i></td>
+              <td style="border-bottom: 1px solid #000000; font-size: 16px; font-weight: bold; color: #d8609e;  " class="donationYearNiceName">DERMA- DERMA DEBIT DIRECT PROGRAMME </td>
+           </tr>
+          </table>
+        </div>
+
+        <div style="font-size:small">
+          <p align="left" style="margin-top:20px;">
+            RM  <span style="border-bottom: 1px solid #000000;padding-left:60px;padding-right:60px;font-size: large;">
+                  <strong>{{$amount}}/=</strong>
+                </span><br>
+            <span id="paymentMode"><strike>Tunai</strike> / Perbankan Internet / <strike>Cek </strike> <br>
+            <strike>Cash</strike> / Internet Banking / <strike>Cheque </strike></span>
+          </p>
+          <p align="right" style="margin-top:-70px;"><img src="http://makna.org.my/images/dato_chop.jpg" height="90" width="280"/></p><br>
+          <p align="right" style="margin-top:-30px; margin-right: 25px;">
+            {{$dato}}<br>
+            {{$president}}<br>
+            {{$makna}}
+          </p>
+          <p style="text-align:center;font-size:12px;background-color: #d8609e; padding: 10px; color: #fff;">
+            <strong>Potongan Di Bawah Subseksyen 44(6) Akta Cukai Pendapatan 1967:&nbsp;  RUJ:LHDN.01/35/42/51/179-6.4232 Berkuatkuasa 1 Mac 1995 &nbsp; No.Warta Kerajaan: 2153 Bertarikh 30 Mac 1995 <br></strong> Majlis Kanser Nasional (MAKNA), BG 03A & 05, Ground Floor, Megan Ambassy,(D – Villa Residence Kuala Lumpur), 225, Jalan Ampang, 50450 Kuala Lumpur
+              Tel: 03-2162 9178 &nbsp;&nbsp;&nbsp; Fax: 03-2162 9203 &nbsp;&nbsp;&nbsp; 
+            
+          </p>
+        </div>
+       
+        <p style="border-bottom: 2px dashed #000000;margin-top:1px;" class="page-break"></p>
+        <p style="font-size:14px; font-weight:bold; color:#000">Derma- DERMA DEBIT DIRECT PROGRAMME</p>
+        <p style="font-size:14px; font-weight:bold; color:#000; margin-top: -10px;">{{$trans_year}} STATEMENT</p>
+
+        
+
+   <div class="row">
+      <div class="col-md-12">
+        @foreach ($receiptGroupByICs as $ic => $receiptGroupByIC)
+        
+        
+        <div class="panel panel-color panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">{{ $ic }}</h3>
+                    
+                    
+                </div>
+                <div class="panel-body">
+                  @php
+                    $groupByMonthYears = $receiptGroupByIC->groupBy(function($receipt) {
+                return \Carbon\Carbon::createFromFormat('Y-m-d', $receipt->trans_date)->format('Y-m');
+
+
+                });
+
+                  @endphp
+
+                    <table class="table table-striped">
+                        <thead style="background-color: #d8609e">
+                            <tr>
+                                <th  class="text-center">MONTH / YEAR</th>
+                                <th class="text-center">AMOUNT (RM)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach ($groupByMonthYears as $monthYear => $groupByMonthYear)
+                          @php
+                            $totalAmount = $groupByMonthYear->sum(function($receipt) {
+                        return $receipt->amount;
+                        
+                });
+                          @endphp
+                            <tr>
+                                <td class="text-center">{{ date('F Y', strtotime($monthYear)) }}</td>
+                                <td class="text-center">{{ number_format($totalAmount, 2) }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+          
+        @endforeach
+
+      
       </div>
-
     </div>
-  </div> -->
 
-</body>
+</body> 
 </html>
-
-@endsection
